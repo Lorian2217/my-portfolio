95f626da40f0e9c990b606923997e06f2195abdb
Loran <danilmockhnatkin@gmail.com>
2026-06-08 19:48:27 +0400

    Исправление ошибок

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,79 +7,66 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: 'https://lorian.su',
       lastModified: new Date(),
       changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
-      priority: 1, // Главная страница самая важная
     },
     {
       url: 'https://lorian.su/services/',
       lastModified: new Date(),
       changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
-      priority: 1, // Главная страница самая важная
     },
     {
       url: 'https://lorian.su/services/razrabotka-saitov/',
       lastModified: new Date(),
       changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
-      priority: 2, // Главная страница самая важная
     },
     {
       url: 'https://lorian.su/services/landing-page/',
       lastModified: new Date(),
       changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
-      priority: 2, // Главная страница самая важная
     },
     {
       url: 'https://lorian.su/services/react-nextjs/',
       lastModified: new Date(),
       changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
-      priority: 2, // Главная страница самая важная
     },
     {
       url: 'https://lorian.su/services/bitrix/',
       lastModified: new Date(),
       changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
-      priority: 2, // Главная страница самая важная
     },
     {
       url: 'https://lorian.su/services/verstka-saita/',
       lastModified: new Date(),
       changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
-      priority: 2, // Главная страница самая важная
     },
     {
       url: 'https://lorian.su/services/tehnicheskaya-podderzhka-saita/',
       lastModified: new Date(),
       changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
-      priority: 2, // Главная страница самая важная
     },
     {
       url: 'https://lorian.su/services/seo-optimizaciya-saita/',
       lastModified: new Date(),
       changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
-      priority: 2, // Главная страница самая важная
     },
     {
       url: 'https://lorian.su/services/integracii-crm/',
       lastModified: new Date(),
       changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
-      priority: 2, // Главная страница самая важная
     },
     {
       url: 'https://lorian.su/projects/',
       lastModified: new Date(),
       changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
-      priority: 1, // Главная страница самая важная
     },
     {
       url: 'https://lorian.su/about/',
       lastModified: new Date(),
       changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
-      priority: 1, // Главная страница самая важная
     },
     {
       url: 'https://lorian.su/contacts/',
       lastModified: new Date(),
       changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
-      priority: 1, // Главная страница самая важная
     },
   ]
 }
