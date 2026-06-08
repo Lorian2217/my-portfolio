5919c0da0dc14653991a3d42792dd6ee6da669c1
Loran <danilmockhnatkin@gmail.com>
2026-06-08 20:10:21 +0400

    Исправление ошибок с sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,55 +5,55 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       // Главная страница
       url: 'https://lorian.su',
-      changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
+      lastModified: "2026-06-08",
     },
     {
       url: 'https://lorian.su/services/',
-      changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
+      lastModified: "2026-06-08",
     },
     {
       url: 'https://lorian.su/services/razrabotka-saitov/',
-      changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
+      lastModified: "2026-06-08",
     },
     {
       url: 'https://lorian.su/services/landing-page/',
-      changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
+      lastModified: "2026-06-08",
     },
     {
       url: 'https://lorian.su/services/react-nextjs/',
-      changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
+      lastModified: "2026-06-08",
     },
     {
       url: 'https://lorian.su/services/bitrix/',
-      changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
+      lastModified: "2026-06-08",
     },
     {
       url: 'https://lorian.su/services/verstka-saita/',
-      changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
+      lastModified: "2026-06-08",
     },
     {
       url: 'https://lorian.su/services/tehnicheskaya-podderzhka-saita/',
-      changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
+      lastModified: "2026-06-08",
     },
     {
       url: 'https://lorian.su/services/seo-optimizaciya-saita/',
-      changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
+      lastModified: "2026-06-08",
     },
     {
       url: 'https://lorian.su/services/integracii-crm/',
-      changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
+      lastModified: "2026-06-08",
     },
     {
       url: 'https://lorian.su/projects/',
-      changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
+      lastModified: "2026-06-08",
     },
     {
       url: 'https://lorian.su/about/',
-      changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
+      lastModified: "2026-06-08",
     },
     {
       url: 'https://lorian.su/contacts/',
-      changeFrequency: 'weekly', // Подсказываем, что сайт обновляется примерно раз в неделю
+      lastModified: "2026-06-08",
     },
   ]
 }
